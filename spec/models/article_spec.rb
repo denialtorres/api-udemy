0621c94456cacf0f10e149e0c5641093bda2375a
Daniel Torres <denial.torres@gmail.com>
2021-12-13 16:17:57 -0600

    recent Article scope with rspec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -29,11 +29,28 @@ RSpec.describe Article, type: :model do
 
     it 'validates uniqueness of the slug' do
       expect(article_two).not_to be_valid
-      expect(article_two.errors[:slug]).to include("has already been taken")
+      expect(article_two.errors[:slug]).to include('has already been taken')
 
       ## generate another slug
       article_two.slug = generate(:slug)
       expect(article_two).to be_valid
+    end
+  end
+
+  describe '.recent' do
+    it 'returns articles correct order' do
+      older_article = create(:article,
+                             created_at: 1.hour.ago)
+      recent_article = create(:article, slug: generate(:slug))
+
+      expect(described_class.recent).to eq(
+        [recent_article, older_article]
+      )
+
+      recent_article.update_column(:created_at, 2.hours.ago)
+      expect(described_class.recent).to eq(
+        [older_article, recent_article]
+      )
     end
   end
 end
