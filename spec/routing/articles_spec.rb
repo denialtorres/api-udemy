38562224a0e01495bf1d400de6f7bd9471391047
Daniel Torres <denial.torres@gmail.com>
2021-12-13 14:18:54 -0600

    rspec for route articles#show

--- spec/routing/articles_spec.rb
+++ spec/routing/articles_spec.rb
@@ -1,10 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe '/articles routes' do
+  let(:article) { create(:article) }
+
   it 'routes to articles#index' do
     aggregate_failures do
       expect(get('/articles')).to route_to('articles#index')
       expect(get('/articles?page[number]=3')).to route_to('articles#index', page: { 'number' => '3' })
     end
+  end
+
+  it 'routes to articles#show' do
+    expect(get("articles/#{article.id}")).to route_to('articles#show', id: "#{article.id}")
   end
 end
