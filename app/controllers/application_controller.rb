85f3618163e4970adb4c2ad5730cdfcdf37c9a9e
Daniel Torres <denial.torres@gmail.com>
2021-12-14 14:57:18 -0600

    Login User Enpoint - Unauthorized

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,18 @@ class ApplicationController < ActionController::API
                             'ActiveRecord::RecordNotFound' => 'JsonapiErrorsHandler::Errors::NotFound'
                           })
   rescue_from ::StandardError, with: ->(e) { handle_error(e) }
+
+  rescue_from UserAuthenticator::AuthenticationError, with: :authentication_error
+
+  private
+
+  def authentication_error
+    error = {
+      status: '401',
+      source: { pointer: '/code' },
+      title: 'Authentication code is invalid',
+      detail: 'You must provide a valid code in order to exchange it for token'
+    }
+    render json: { 'errors' => [error] }, status: 401
+  end
 end
