689249c1d924862bfd328ccb5ff1860eeb6341b3
Daniel Torres <denial.torres@gmail.com>
2021-12-14 17:03:57 -0600

    Authorization error handler

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::API
+  class AuthorizationError < StandardError; end
+
   include JsonapiErrorsHandler
   ErrorMapper.map_errors!({
                             'ActiveRecord::RecordNotFound' => 'JsonapiErrorsHandler::Errors::NotFound'
@@ -6,6 +8,7 @@ class ApplicationController < ActionController::API
   rescue_from ::StandardError, with: ->(e) { handle_error(e) }
 
   rescue_from UserAuthenticator::AuthenticationError, with: :authentication_error
+  rescue_from AuthorizationError, with: :authorization_error
 
   private
 
@@ -17,5 +20,15 @@ class ApplicationController < ActionController::API
       detail: 'You must provide a valid code in order to exchange it for token'
     }
     render json: { 'errors' => [error] }, status: 401
+  end
+
+  def authorization_error
+    error = {
+      status: '403',
+      source: { pointer: '/headers/authorization' },
+      title: 'Not authorized',
+      detail: 'You have no right to access this resource'
+    }
+    render json: { 'errors' => [error] }, status: 403
   end
 end
