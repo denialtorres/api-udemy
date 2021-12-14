c8ff6c6de9a502156bbd6582e810f2667c983390
Daniel Torres <denial.torres@gmail.com>
2021-12-14 13:10:18 -0600

    UserAuthenticator service - success

--- app/lib/user_authenticator.rb
+++ app/lib/user_authenticator.rb
@@ -2,12 +2,28 @@
 class UserAuthenticator
   class AuthenticationError < StandardError; end
 
-  attr_reader :user
+  attr_reader :user, :code
 
   def initialize(code)
+    @code = code
   end
 
   def perform
-    raise AuthenticationError
+    client = Octokit::Client.new(
+      client_id: 'xxxx',
+      client_secret: 'xxx'
+    )
+
+    token = client.exchange_code_for_token(code)
+
+    if token.try(:error).present?
+      raise AuthenticationError
+    else
+      user_client = Octokit::Client.new(
+        access_token: token
+      )
+      user_data = user_client.user.to_h.slice(:login, :avatar, :url, :name)
+      User.create(user_data.merge(provider: 'github'))
+    end
   end
 end
