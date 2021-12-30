ca5259ce82f2872aa79657aaf90a39b22130529a
Daniel Torres <denial.torres@gmail.com>
2021-12-30 16:07:32 -0600

    custom excludes

--- spec/models/access_token_spec.rb
+++ spec/models/access_token_spec.rb
@@ -3,14 +3,6 @@
 require "rails_helper"
 
 RSpec.describe AccessToken, type: :model do
-  describe "#validations" do
-    it "should have valid factory" do
-    end
-
-    it "should validate token" do
-    end
-  end
-
   describe "#new" do
     it "should have a token present after initialize" do
       expect(AccessToken.new.token).to be_present
